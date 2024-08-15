d69bbd2cfdd541348679e702469ec2f597596bd4
stuffstuff554 <stuff.stuff554@outlook.com>
2024-08-15 21:05:45 +0930

    modified:   script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 async function sendMessage(message) {
     try {
-        const response = await fetch('http://127.0.0.1:5000/process', {
+        const response = await fetch('http://epix-service.duckdns.org:5000/process', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
